523e2c4573dab12c22c0e57c1fbde75052fd6604
Chang An <1753632@tongji.edu.cn>
2024-03-27 01:49:39 +0800

    start dev

---
+++ main.py
@@ -0,0 +1,10 @@
+# 2024.3.26 created by: An Chang
+
+from src.base.Frame import Frame, FrameSeries
+from src.base.Physics import Acc, Vel, Disp, Dimension, Timestamp
+from src.base.MathUtils import MergeInterval
+
+
+if __name__ == '__main__':
+    print("running")
+    pass
---
+++ src/base/MathUtils.py
@@ -0,0 +1,10 @@
+# 2024.3.26 created by: An Chang
+
+def FindMaxGrad():
+    pass
+
+def RandomDistribute():
+    pass
+
+def MergeInterval():
+    pass
