58c7f2d12d85dfedc5006dbd5756ea50830defe5
Chang An <1753632@tongji.edu.cn>
2024-05-08 10:30:09 +0800

    edit md

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # 2024.3.26 created by: An Chang
 
-from src.base.frame import Frame, FrameSeries
+from src.base.Frame import Frame, FrameSeries
 from src.base.physics import Acc, Vel, Disp, Dimension, Timestamp
 from src.common.math_utils import MergeInterval
 from src.common.dataloader import Loader, Parser
